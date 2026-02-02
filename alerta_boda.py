747f644654931437924081fcba816104623b7e04
Alfredo Mosquera <aimp061@gmail.com>
2026-02-02 11:51:15 -0300

    Actualización de apuntes

--- alerta_boda.py
+++ alerta_boda.py
@@ -2,13 +2,13 @@ import datetime
 import ctypes
 
 # Configuración de la fecha
-fecha_boda = datetime.date(2026, 3, 26) 
+fecha_boda = datetime.date(2026, 3, 27) 
 hoy = datetime.date.today()
 dias_faltantes = (fecha_boda - hoy).days
 
 # Configuración del mensaje
 titulo = "Cuenta Regresiva para la Boda"
-mensaje = f"¡Hola! Quedan {dias_faltantes} días para el 26 de marzo.\n\n Amanda y Alfredo"
+mensaje = f"¡Hola! Quedan {dias_faltantes} días para el 27 de marzo.\n\n Amanda y Alfredo"
 
 # Mostrar alerta en Windows
 if dias_faltantes >= 0:
